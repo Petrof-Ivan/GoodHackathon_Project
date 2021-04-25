8406d9134676363c0d70527a9308169ab80ce7e3
forger01 <soketlop@gmail.com>
2021-04-25 18:30:43 +0300

    some bad code needed to be repaired. just for updating local repo after hard reset

--- placeReviewBot/main/management/commands/bot.py
+++ placeReviewBot/main/management/commands/bot.py
@@ -1,11 +1,18 @@
 from django.core.management.base import BaseCommand
 import telegram
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler, CallbackQueryHandler
 from telegram.utils.request import Request
-from telegram.ext import MessageHandler, Filters, CallbackContext
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
+import logging
 
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+FEEDBACK, PHOTO, LOCATION = range(3)
 
 #simple error handler
 def log_errors(f):
@@ -28,6 +35,117 @@ def handle_message(update : Update, context: CallbackContext):
     update.message.reply_text(text = reply_text)
 
 
+def start(update: Update, _: CallbackContext) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("Оставить отзыв", callback_data='1'),
+            InlineKeyboardButton("Почитать отзывы", callback_data='2'),
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Привет, я бот который поможет оставить отзыв о благоустроенной территории. '
+                              'Выбери что хочешь сделать: Набери /cancel чтобы перестать со мной общаться\n\n',
+                              reply_markup=reply_markup)
+
+
+def help_command(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text("Пожалуйста, выберите что вы хотите сделать")
+
+
+def feedback(update: Update, _: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Get feedback of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(
+        'Круто. Теперь отправь мне пожалуйста фото территории, '
+        'или нажми /skip чтобы пропустить',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PHOTO
+
+
+
+def button(update: Update, _: CallbackContext) -> None:
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    query.edit_message_text(text=f"Selected option: {query.data}")
+    if query.data == '2':
+        query.edit_message_text(text=f"Пока этот раздел не доступен в демо версии бота."
+                                     f" Возможно он появится позже, и Вы сможете читать комментарии"
+                                     f" Нажмите /start чтобы начать сначала")
+    if query.data == '1':
+        feedback
+
+
+def photo(update: Update, _: CallbackContext) -> int:
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text(
+        'Gorgeous! Now, send me your location please, or send /skip if you don\'t want to.'
+    )
+
+    return LOCATION
+
+
+def skip_photo(update: Update, _: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    update.message.reply_text(
+        'I bet you look great! Now, send me your location please, or send /skip.'
+    )
+
+    return LOCATION
+
+
+def location(update: Update, _: CallbackContext) -> int:
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    update.message.reply_text(
+        'Maybe I can visit you sometime! At last, tell me something about yourself.'
+    )
+
+    return ConversationHandler.END
+
+
+def skip_location(update: Update, _: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    update.message.reply_text(
+        'You seem a bit paranoid! At last, tell me something about yourself.'
+    )
+
+    return ConversationHandler.END
+
+
+def bio(update: Update, _: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! I hope we can talk again some day.')
+
+    return ConversationHandler.END
+
+
+def cancel(update: Update, _: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
 class Command(BaseCommand):
     help = 'Telegram-bot'
 
@@ -38,15 +156,39 @@ class Command(BaseCommand):
         f = open('token.txt')
         token = f.read()
 
+        updater = Updater(token)
+
+        # Get the dispatcher to register handlers
+        dispatcher = updater.dispatcher
+        # начало работы бота, начинаем с команды /start
+        updater.dispatcher.add_handler(CommandHandler('start', start))
+        updater.dispatcher.add_handler(CallbackQueryHandler(button))
+        updater.dispatcher.add_handler(CommandHandler('help', help_command))
+        # Add conversation handler with the states FEEDBACK, PHOTO, LOCATION
+        conv_handler = ConversationHandler(
+            entry_points=[MessageHandler(Filters.text & ~Filters.command, feedback)],
+            states={
+                FEEDBACK: [MessageHandler(Filters.text & ~Filters.command, feedback)],
+                PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
+                LOCATION: [
+                    MessageHandler(Filters.text & Filters.location, location),
+                    CommandHandler('skip', skip_location),
+                ]
+            },
+            fallbacks=[CommandHandler('cancel', cancel)],
+        )
+
+        dispatcher.add_handler(conv_handler)
+
         # creating the bot
-        bot = telegram.Bot(token=token, request=request)
+        #bot = telegram.Bot(token=token, request=request)
 
         # run this to see bot parameters
         # print(bot.get_me())
 
-        updater = Updater(token=token, use_context=True)
-        message_handler = MessageHandler(Filters.text, handle_message)
-        updater.dispatcher.add_handler(message_handler)
+        #updater = Updater(token=token, use_context=True)
+        #message_handler = MessageHandler(Filters.text, handle_message)
+        #updater.dispatcher.add_handler(message_handler)
 
         updater.start_polling()
         updater.idle()
